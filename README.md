fb15c79b9b6ab1b7e652a179439b0daa3a5c11a0
Ayush Singh <ayushsingh.as1700@gmail.com>
2021-10-05 04:32:03 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Decentralized-Photo-Sharing-Tipping-Using-Eth
+# Decentralized-Photo-Sharing-Tipping-Using-Eth
---
+++ test/test.js
@@ -0,0 +1,56 @@
+const { assert } = require('chai')
+
+const Decentragram = artifacts.require('./Decentragram.sol')
+
+require('chai')
+  .use(require('chai-as-promised'))
+  .should()
+
+contract('Decentragram', ([deployer, author, tipper]) => {
+  let decentragram
+
+  before(async () => {
+    decentragram = await Decentragram.deployed()
+  })
+
+  describe('deployment', async () => {
+    it('deploys successfully', async () => {
+      const address = await decentragram.address
+      assert.notEqual(address, 0x0)
+      assert.notEqual(address, '')
+      assert.notEqual(address, null)
+      assert.notEqual(address, undefined)
+    })
+
+    it('has a name', async () => {
+      const name = await decentragram.name()
+      assert.equal(name, 'Decentragram')
+    })
+  })
+
+  describe('images', async() => {
+    let result
+    let imageCount
+    const hash = 'abc123'
+
+    before(async () => {
+      result = await decentragram.uploadImage(hash, 'Image description', {from: author})
+      imageCount = await decentragram.imageCount()
+    })
+
+    it('create images', async() => {
+      //Success
+      assert.equal(imageCount, 1)
+      const event = result.logs[0].args
+      assert.equal(event.id.toNumber(), imageCount.toNumber(), 'id is correct')
+      assert.equal(event.hash, hash, 'hash is correct')
+      
+      //Failure: Image must have hash
+      await decentragram.uploadImage('', 'Image description', {from: author}).should.be.rejected;
+      await decentragram.uploadImage('Image Hash', '', {from: author}).should.be.rejected;
+    })
+
+    // Check from struct
+    
+  })
+})
